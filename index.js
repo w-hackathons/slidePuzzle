8957173b2053ff07799f6b9b5fa87f6ed5c61d7a
Tukudani <zin9349@outlook.com>
2022-03-04 20:06:43 +0900

    add:ひながた

---
+++ index.js
@@ -0,0 +1,3 @@
+const sampleImg = "https://3.bp.blogspot.com/-8tO27OaQmKo/W1vhDEBgLMI/AAAAAAABNtc/WqKk_bLT1k0O2loY5VH5UeNp6ejZ3GBwQCLcBGAs/s800/character_earth_chikyu_internet.png";
+const imageArea = document.getElementById("image");
+imageArea.src = sampleImg;
