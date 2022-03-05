e4066b16c8942ef0624149a6b154094256e3207b
o1hashimoto <ray.out.036@gmail.com>
2022-03-05 17:38:52 +0900

    temporary

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const sampleImg = "https://3.bp.blogspot.com/-8tO27OaQmKo/W1vhDEBgLMI/AAAAAAABNtc/WqKk_bLT1k0O2loY5VH5UeNp6ejZ3GBwQCLcBGAs/s800/character_earth_chikyu_internet.png";
-const imageArea = document.getElementById("image");
-imageArea.src = sampleImg;
+// const sampleImg = "https://3.bp.blogspot.com/-8tO27OaQmKo/W1vhDEBgLMI/AAAAAAABNtc/WqKk_bLT1k0O2loY5VH5UeNp6ejZ3GBwQCLcBGAs/s800/character_earth_chikyu_internet.png";
+// const imageArea = document.getElementById("image");
+// imageArea.src = sampleImg;
 
 
 
-// 画像のリサイズを行う関数
-// direction=縦横　0なら横のサイズ、1なら縦のサイズを指定する
-function imageResize(direction, size){
-  const targetImage = document.getElementById("image");
-  const orgWidth  = targetImage.width;  // 元の横幅を保存
-  const orgHeight = targetImage.height; // 元の高さを保存
-  if(direction == 0){
-    targetImage.width = size;  // 横幅を400pxにリサイズ
-    targetImage.height = orgHeight * (targetImage.width / orgWidth);
-  }else{
-    targetImage.height = size;  // 横幅を400pxにリサイズ
-    targetImage.Width = orgWidth * (targetImage.height / height);
-  }
-}
+// // 画像のリサイズを行う関数
+// // direction=縦横　0なら横のサイズ、1なら縦のサイズを指定する
+// function imageResize(direction, size){
+//   const targetImage = document.getElementById("image");
+//   const orgWidth  = targetImage.width;  // 元の横幅を保存
+//   const orgHeight = targetImage.height; // 元の高さを保存
+//   if(direction == 0){
+//     targetImage.width = size;  // 横幅を400pxにリサイズ
+//     targetImage.height = orgHeight * (targetImage.width / orgWidth);
+//   }else{
+//     targetImage.height = size;  // 横幅を400pxにリサイズ
+//     targetImage.Width = orgWidth * (targetImage.height / height);
+//     }
+// }
 
-imageResize(1,400);
+// imageResize(1,400);
